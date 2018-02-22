093edf104ce492e2ee01fed2fb4b3f132347b48e
Memnoc <smartdroidesign@gmail.com>
2018-02-22 22:45:46 +0000

    - methods

--- src/Methods/MethodChallenge.java
+++ src/Methods/MethodChallenge.java
@@ -1,11 +1,36 @@
 package Methods;
 
+// Create a method called displayHighScorePosition
+// It should take a playerName as parameter, and a 2nd parameter as a position in the high score table
+// You should display the players name along with a message like " managed to get into position" and the
+// position they got and a further message " on the high score table".
+//
+// Create a 2nd method called calculateHighScorePosition
+// it should be sent one argument only, the player score
+// it should return an int
+// the return data should be:
+// 1 if the score is > 1000
+// 2 if the score is > 500 and < 1000
+// 3 if the score is > 100 and < 500
+// 4 in all other cases
+// call both methods and display the result of the following
+// a score of 1500, 900, 400 and 50
+
 public class MethodChallenge {
 
     public static void main(String[] args) {
 
+        int highScorePosition = displayHighScorePosition(1500);
+        displayHighScore("Tim", highScorePosition);
 
+        highScorePosition = displayHighScorePosition(900);
+        displayHighScore("Mark", highScorePosition);
 
+        highScorePosition = displayHighScorePosition(400);
+        displayHighScore("Julian", highScorePosition);
+
+        highScorePosition = displayHighScorePosition(50);
+        displayHighScore("Holly", highScorePosition);
 
 
     }
@@ -15,11 +40,11 @@ public class MethodChallenge {
     }
 
     public static int displayHighScorePosition ( int playerScore) {
-        if (playerScore > 1000) {
+        if (playerScore >= 1000) {
             return 1;
-        } else if ((playerScore > 500) && (playerScore < 1000)) {
+        } else if ((playerScore >= 500) && (playerScore < 1000)) {
             return 2;
-        } else if ((playerScore > 100) && (playerScore < 500)) {
+        } else if ((playerScore >= 100) && (playerScore < 500)) {
             return  3;
         } else {
             return 4;
---
+++ src/Methods/MethodOverloading.java
@@ -0,0 +1,25 @@
+package Methods;
+
+// Method overloading is the option where you can use the same method name with different parameters
+
+public abstract class MethodOverloading {
+
+
+
+    public static void main(String[] args) {
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(100);
+    }
+
+    public static int calculateScore (String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return  score * 1000;
+    }
+
+    public static int calculateScore (int score) {
+        System.out.println("Player " + " scored " + score + " points");
+        return  score * 1000;
+    }
+
+}
