8de89bd93f80cfbeff1c1c292bcb0d11f77040c7
Memnoc <mgenesiostara@zendesk.com>
2018-02-23 13:15:12 +0000

    - method overloading challenge

--- src/Methods/MethodOverloading.java
+++ src/Methods/MethodOverloading.java
@@ -1,6 +1,7 @@
 package Methods;
 
 // Method overloading is the option where you can use the same method name with different parameters
+// Overloading a method means using a different signature, same name, but different parameters.
 
 public abstract class MethodOverloading {
 
@@ -20,6 +21,11 @@ public abstract class MethodOverloading {
     public static int calculateScore (int score) {
         System.out.println("Player " + " scored " + score + " points");
         return  score * 1000;
+    }
+
+    public static int calculateScore () {
+        System.out.println("No player name, no player score");
+        return  0;
     }
 
 }
