a52a445feddb65e0d00ce456d5452ef0d94121e2
Memnoc <smartdroidesign@gmail.com>
2018-04-03 19:46:10 +0100

    - Generics part 3

--- src/Generics/Team.java
+++ src/Generics/Team.java
@@ -2,7 +2,7 @@ package Generics;
 
 import java.util.ArrayList;
 
-public class Team<T extends Player> {
+public class Team<T extends Player> implements Comparable<Team<T>> {
     private String teamName;
     int played = 0;
     int won = 0;
@@ -19,7 +19,7 @@ public class Team<T extends Player> {
         return teamName;
     }
 
-    public boolean addPlayer (T player) {
+    public boolean addPlayer(T player) {
         if (members.contains(player)) {
             System.out.println(player.getPlayerName() + " is already on the team");
             return false;
@@ -37,10 +37,10 @@ public class Team<T extends Player> {
     public void matchResult(Team<T> opponent, int ourScore, int theirScore) {
         String message;
 
-        if (ourScore > theirScore){
+        if (ourScore > theirScore) {
             won++;
             message = " beat ";
-        } else if(ourScore == theirScore){
+        } else if (ourScore == theirScore) {
             tied++;
             message = " drew with ";
         } else {
@@ -48,7 +48,7 @@ public class Team<T extends Player> {
             message = " lost to ";
         }
         played++;
-        if (opponent != null){
+        if (opponent != null) {
             System.out.println(this.getTeamName() + message + opponent.getTeamName());
             opponent.matchResult(null, theirScore, ourScore);
         }
@@ -56,5 +56,17 @@ public class Team<T extends Player> {
 
     public int ranking() {
         return (won * 2) + tied;
+    }
+
+
+    @Override
+    public int compareTo(Team<T> team) {
+        if (this.ranking() > team.ranking()) {
+            return -1;
+        } else if (this.ranking() < team.ranking()) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
