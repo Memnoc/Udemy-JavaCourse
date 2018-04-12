f4dbf292921df3b93b689f71182a9fdb2e10504f
Memnoc <mgenesiostara@zendesk.com>
2018-04-12 15:23:24 +0100

    - challenge completed

--- src/Packages/com/smartdroid/challenge/Series.java
+++ src/Packages/com/smartdroid/challenge/Series.java
@@ -10,6 +10,32 @@ public class Series {
         }
     }
 
+    public static void factorial2(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+            System.out.println(fact);
+        }
+    }
+
+
+    public static void fibonacci(int n) {
+        int a = 0;
+        int b = 1;
+        int temp = a+b;
+
+
+        for (int i = 0; i < n; i++) {
+            temp = (a + b);
+            b = a;
+            a = temp;
+            System.out.println(temp);
+
+
+        }
+    }
+
+
 
 
 }
