130890bcb0ce4bcbc2e990ed68ea891c296a631e
Memnoc <smartdroidesign@gmail.com>
2018-04-12 18:48:15 +0100

    - Scope part 2 - finished

--- src/Scope/Main.java
+++ src/Scope/Main.java
@@ -3,13 +3,23 @@ package Scope;
 public class Main {
 
     public static void main(String[] args) {
-        String privateVar = "this is private to main()";
+        String varFour = "this is private to main()";
 
         ScopeCheck scopeInstance = new ScopeCheck();
-        System.out.println("scopeInstance privateVar is " + scopeInstance.getPrivateVar());
-        System.out.println(privateVar);
+        scopeInstance.useInner();
+        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+        //System.out.println("varThree is not accessible here " + innerClass.varThree);
 
-        scopeInstance.timesTwo();
+
+//        System.out.println("scopeInstance varOne is " + scopeInstance.getVarOne());
+//        System.out.println(varFour);
+//
+//        scopeInstance.timesTwo();
+//        System.out.println("================");
+//        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+//        innerClass.timesTwo();
+
+
 
     }
 }
