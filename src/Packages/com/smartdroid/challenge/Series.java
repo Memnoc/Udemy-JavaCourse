c76d0f697367aba3554447d0680ed9f7cbf99a8e
Memnoc <mgenesiostara@zendesk.com>
2018-04-12 14:20:19 +0100

    - packages challenge - first method

--- src/Packages/com/smartdroid/challenge/Series.java
+++ src/Packages/com/smartdroid/challenge/Series.java
@@ -2,5 +2,14 @@ package Packages.com.smartdroid.challenge;
 
 public class Series {
 
+    public static void nSum() {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+            System.out.println(sum);
+        }
+    }
+
+
 
 }
