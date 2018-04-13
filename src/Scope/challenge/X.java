1a5d70057317d6b015a3a4788ca1fe414dc8274f
Memnoc <mgenesiostara@zendesk.com>
2018-04-13 16:17:46 +0100

    - scope challenge completed

---
+++ src/Scope/challenge/X.java
@@ -0,0 +1,36 @@
+package Scope.challenge;
+
+import java.util.Scanner;
+
+public class X {
+
+    private int x = 0;
+
+    public static void main(String[] args) {
+
+        X x = new X();
+        x.x();
+
+
+    }
+
+
+    private void x() {
+        System.out.println("please enter a number");
+        x x = new x();
+        x.x = 2;
+        for (this.x = 1; this.x <= 12; this.x++) {
+            System.out.println(this.x * x.x);
+
+        }
+    }
+
+    private class x {
+
+        private int x = 0;
+        private x() {
+            Scanner x = new Scanner(System.in);
+            x.nextInt();
+        }
+    }
+}
