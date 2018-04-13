8f84f9670181b0be7630ff9498d580e929ef4e92
Memnoc <smartdroidesign@gmail.com>
2018-04-13 19:44:51 +0100

    - Scope challenge - refactored

--- src/Scope/challenge/X.java
+++ src/Scope/challenge/X.java
@@ -1,36 +1,20 @@
 package Scope.challenge;
 
-import java.util.Scanner;
-
 public class X {
 
     private int x = 0;
 
-    public static void main(String[] args) {
-
-        X x = new X();
-        x.x();
-
-
+    public X(int x) {
+        this.x = x;
     }
 
-
-    private void x() {
-        System.out.println("please enter a number");
-        x x = new x();
-        x.x = 2;
-        for (this.x = 1; this.x <= 12; this.x++) {
-            System.out.println(this.x * x.x);
+    public void x() {
+        int x = 2;
+        for (x = 1; x <= 12; x++) {
+            System.out.println(this.x * x);
 
         }
     }
 
-    private class x {
 
-        private int x = 0;
-        private x() {
-            Scanner x = new Scanner(System.in);
-            x.nextInt();
-        }
-    }
 }
