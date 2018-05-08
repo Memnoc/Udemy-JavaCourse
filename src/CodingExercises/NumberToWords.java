6887fad891bb3066f4825589c35af8d07f332017
Memnoc <mgenesiostara@zendesk.com>
2018-05-08 16:11:35 +0100

    - Coding Exercises 4

--- src/CodingExercises/NumberToWords.java
+++ src/CodingExercises/NumberToWords.java
@@ -4,7 +4,90 @@ public class NumberToWords {
 
     public static void main(String[] args) {
 
+        System.out.println(getDigitCount(100));
+//        System.out.println(getDigitCount(123));
+//        System.out.println(getDigitCount(-12));
+//        System.out.println(getDigitCount(5200));
+
+//        System.out.println(reverse(-121));
+//        System.out.println(reverse(1212));
+//        System.out.println(reverse(1234));
+//        System.out.println(reverse(1));
+        System.out.println(reverse(100));
+
+//        System.out.println(numberToWords(1235567));
+//        System.out.println(numberToWords(1010));
+//        System.out.println(numberToWords(1000));
+//        System.out.println(numberToWords(-12));
+        System.out.println(numberToWords(100));
+
+    }
+
+    public static String numberToWords(int number) {
+        int numberReversed = reverse(number);
+        if (numberReversed < 0) {
+            return "Invalid value";
+        }
+        StringBuilder str = new StringBuilder("");
+        while (numberReversed != 0) {
+            int lastDigit = numberReversed % 10;
+            switch (lastDigit) {
+                case 0:
+                    str.append("Zero\n");
+                    break;
+                case 1:
+                    str.append("One\n");
+                    break;
+                case 2:
+                    str.append("Two\n");
+                    break;
+                case 3:
+                    str.append("Three\n");
+                    break;
+                case 4:
+                    str.append("Four\n");
+                    break;
+                case 5:
+                    str.append("Five\n");
+                    break;
+                case 6:
+                    str.append("Six\n");
+                    break;
+                case 7:
+                    str.append("Seven\n");
+                    break;
+                case 8:
+                    str.append("Eight\n");
+                    break;
+                case 9:
+                    str.append("Nine\n");
+                    break;
+            }
+            numberReversed /= 10;
+        }
+        return String.valueOf(str);
+    }
+
+
+    public static int reverse(int value) {
+        int resultNumber = 0;
+        for (int i = value; i != 0; i /= 10) {
+            resultNumber = resultNumber * 10 + i % 10;
+
+        }
+        return resultNumber;
+    }
+
+    public static int getDigitCount(int number) {
+        if (number < 0)
+            return - 1;
+        return String.valueOf(number).length();
     }
 
 
 }
+
+
+
+
+
