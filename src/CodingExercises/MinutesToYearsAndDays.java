2eaaa9e629c2cf76ad3db043bb2db8fa7c3a4113
Memnoc <smartdroidesign@gmail.com>
2018-06-11 15:52:51 +0100

    Minutes to years and days coding challenge

---
+++ src/CodingExercises/MinutesToYearsAndDays.java
@@ -0,0 +1,23 @@
+package CodingExercises;
+
+public class MinutesToYearsAndDays {
+
+    public static void main(String[] args) {
+
+       printYearsAndDays(525600);
+       printYearsAndDays(1051200);
+       printYearsAndDays(561600);
+
+    }
+
+    public static void printYearsAndDays (long minutes){
+        long year = minutes / 525600;
+        long days = (minutes % 525600) / 1440;
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(minutes + " min = " + year + " y and " + days + " d");
+        }
+    }
+
+}
