331b186dc1c12d8617be7a808d72c77a0fdb57cd
Memnoc <smartdroidesign@gmail.com>
2018-06-25 15:57:31 +0100

    Threads - Deadlocks, wait, notify and notifyAll methods

--- src/Threads/ProducerAndConsumer/Main.java
+++ src/Threads/ProducerAndConsumer/Main.java
@@ -9,6 +9,9 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
 
     }
 }
@@ -20,20 +23,29 @@ class Message {
 
     public synchronized String read() {
         while (empty) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
 
+            }
         }
         empty = true;
+        notifyAll();
         return message;
     }
 
     public synchronized void write(String message) {
         while (!empty) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
 
+            }
         }
         empty = false;
         this.message = message;
+        notifyAll();
     }
-
 }
 
 class Writer implements Runnable {
@@ -62,5 +74,28 @@ class Writer implements Runnable {
             }
         }
         message.write("Finish");
+    }
+}
+
+class Reader implements Runnable {
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        Random random = new Random();
+        for (String latestMessage = message.read(); !latestMessage.equals("Finished");
+            latestMessage = message.read()) {
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e) {
+
+            }
+
+        }
+
     }
 }
