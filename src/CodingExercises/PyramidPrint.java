734b25e0e1822c64f34a44613d1c1a18926ad925
Memnoc <smartdroidesign@gmail.com>
2019-01-28 17:44:12 +0000

    pyramid print

---
+++ src/CodingExercises/PyramidPrint.java
@@ -0,0 +1,18 @@
+package CodingExercises;
+
+public class PyramidPrint {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 5 ; i++) {
+            System.out.println();
+            for (int j = 0; j <= 5-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <=i; k++) {
+                System.out.print("* ");
+            }
+        }
+    }
+
+}
